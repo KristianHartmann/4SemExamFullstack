9492d403c6aaf695c11062c73d7138fdce65f571
KristianHartmann <32513300+KristianHartmann@users.noreply.github.com>
2023-03-02 09:50:33 +0100

    Updated header components

---
+++ 4SemExamFullstack/src/LoginModal.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { Modal, Button, Form } from "react-bootstrap";
+import "./header.css";
+
+type Props = {
+  show: boolean;
+  handleClose: () => void;
+};
+
+const LoginModal: React.FC<Props> = ({ show, handleClose }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // Handle login logic here
+    // ...
+    // After successful login, close the modal
+    handleClose();
+  };
+
+  return (
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Login</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Modal.Body>
+    </Modal>
+  );
+};
+
+export default LoginModal;
