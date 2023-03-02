f298687bc1ef5e03a58aefe4110001672d13e518
Frederik Hess <cph-fp103@cphbusiness.dk>
2023-03-02 10:38:33 +0100

    implemented some footer work

--- 4SemExamFullstack/src/components/Footer.tsx
+++ 4SemExamFullstack/src/components/Footer.tsx
@@ -1,7 +1,46 @@
 import React from "react";
+import instagramLogo from "../images/instagramlogo.png";
+import facebookLogo from "../images/facebooklogo.png";
+import twitterLogo from "../images/twitterlogo.png";
+import companyLogo from "../images/monkeyLogo.png";
 
-const Footer = () => {
-  return <div></div>;
+import "../styles/Footer.css";
+
+const Footer: React.FC = () => {
+  return (
+    <footer>
+      <nav>
+        <ul>
+          <li>
+            <a href="https://www.facebook.com/">
+              <img src={facebookLogo} alt="Facebook" />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.twitter.com/">
+              <img src={twitterLogo} alt="Twitter" />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.instagram.com/">
+              <img src={instagramLogo} alt="Instagram" />
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <div className="contact-info">
+        <div>Email: contact@CookingWithMonkeys.dk</div>
+        <div>Phone: 12344321</div>
+        <div>Address: 123 Monkey St, Denmark</div>
+      </div>
+      <div className="company-logo">
+        <img src={companyLogo} alt="Company Logo" />
+      </div>
+      <div className="copyright">
+        &copy; {new Date().getFullYear()} Cooking With Monkeys
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
