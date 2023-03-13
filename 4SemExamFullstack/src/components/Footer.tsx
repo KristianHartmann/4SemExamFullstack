289933a1d2ccdaad70731c43d3b1bac82ba18e42
KristianHartmann <krha998@yahoo.dk>
2023-03-13 23:49:14 +0100

    added searchbar

--- 4SemExamFullstack/src/components/Footer.tsx
+++ 4SemExamFullstack/src/components/Footer.tsx
@@ -7,31 +7,25 @@ import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg
 
 const Footer: React.FC = () => {
   return (
-    <footer className="flex justify-between text-white items-center w-full bg-footer sm:px-16 px-6 mb-auto flex-shrink-0 mt-auto font-serif">
-      <div className="flex h-10 gap-6">
-      
-    
+    <footer className="flex justify-between text-white items-center w-full bg-footer sm:px-16 font-serif">
+      <div className="flex h-10 gap-6 py-2 ml-9">
       <a href="https://www.facebook.com/">
         <FontAwesomeIcon icon={faFacebook} size="2x"/>
         </a>
-
-        <a href="https://www.twitter.com/">
+       <a href="https://www.twitter.com/">
         <FontAwesomeIcon icon={faTwitter} size="2x"/>
         </a>
-        <a href="https://www.instagram.com/">
+       <a href="https://www.instagram.com/">
         <FontAwesomeIcon icon={faInstagram} size="2x"/>
         </a>
-     
       </div>
       <div className="py-2">
         <div>Email: contact@CookingWithMonkeys.dk</div>
         <div>Phone: 12344321</div>
         <div>Address: 123 Monkey St, Denmark</div>
       </div>
-      <div className="h-10 items-center">
-        <div className="copyright">
+      <div className="h-10p py-2">
           &copy; {new Date().getFullYear()} Cooking With Monkeys
-        </div>
       </div>
     </footer>
   );
--- 4SemExamFullstack/src/components/SearchRecipes.tsx
+++ 4SemExamFullstack/src/components/SearchRecipes.tsx
@@ -1,18 +1,72 @@
 import React from "react";
 import { useState } from "react";
 import { SearchRecipesText } from "../constants";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faSearch,
+} from "@fortawesome/free-solid-svg-icons";
 const SearchRecipes = () => {
+  const [searchText, setSearchText] = useState("");
+  const [filters, setFilters] = useState({
+    vegetarian: false,
+    glutenFree: false,
+    dairyFree: false,
+  });
+
+  
+
+   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("searched");
+    console.log(filters);
+  };
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchText(e.target.value);
+  };
+
+  const handleCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setSearchText(e.target.value);
+    };  
+  };
+
   return (
-    <section className="flex justify-center items-center bg-white p-5">
-      <div className="flex flex-col heading2 text-center ">
-      {
-      SearchRecipesText.find((text) => text.id === "SearchRecipesText")
+    <section>
+
+   
+      <div className="p-5 heading2 bg-white w-1/2 mx-auto h-96 ">
+        <div className="mb-5 font-sans">
+        {
+        SearchRecipesText.find((text) => text.id === "SearchRecipesText")
         ?.heading
-      }
+        }
         </div>
-    
-    </section>
+  
+        <form onSubmit={handleSubmit}>
+           <div className="relative">
+            <div className="absolute inset-y-0 left-0 flex items-center pl-2 pointer-events-none ">
+              <FontAwesomeIcon
+              icon={faSearch}
+                className="w-28px h-28px object-contain text-gray-500"
+         />
+        </div>
+        <input
+          type="search"
+          id="default-search"
+          className="block w-full p-4 pl-10 text-sm border border-gray-300  rounded-lg bg-gray-50 outline-none"
+          placeholder="Search recipes..."
+          required
+        />
+        <button
+          type="submit"
+          className="text-white absolute right-2.5 bottom-2.5 bg-primary hover:bg-tertiary font-medium rounded-lg text-sm px-4 py-2">
+          Search
+        </button>
+      </div>
+    </form>
+
+        </div>
+        </section>
   
 
   );
