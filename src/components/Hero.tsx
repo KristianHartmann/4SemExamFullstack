7ad7f4487d18707ce69dd9da9f260eb9418d2924
KristianHartmann <32513300+KristianHartmann@users.noreply.github.com>
2023-03-16 11:09:43 +0100

    fixed some styling errors to hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -14,19 +14,27 @@ function Hero() {
   const { heading, textOne, textTwo, linkText } = bannerText;
 
   return (
-    <section id="home" className="bg-tertiary flex text-white font-serif">
-      <div className="flex flex-col w-1/2 items-center flex-grow gap-14">
-        <div className="heading1 flex justify-center pt-3 mt-16">{heading}</div>
-        <div className="flex justify-center w-1/2 text-2xl">{textOne}</div>
-        <div className="flex justify-center w-1/2 text-2xl ">{textTwo}</div>
+    <section
+      id="home"
+      className="bg-tertiary flex flex-col md:flex-row text-white font-serif">
+      <div className="flex flex-col w-full md:w-5/12 items-center flex-grow gap-8">
+        <div className="heading1 flex justify-center pt-3 mt-16 text-xl lg:text-2xl">
+          {heading}
+        </div>
+        <div className="flex justify-center w-5/12 md:w-1/2 text-xl lg:text-2xl">
+          {textOne}
+        </div>
+        <div className="flex justify-center w-5/12 md:w-1/2 text-xl lg:text-2xl">
+          {textTwo}
+        </div>
         <div>
           <Link to="searchrecipes">
             <button className="btn-primary">{linkText}</button>
           </Link>
         </div>
       </div>
-      <div className="flex flex-col flex-grow w-1/2 h-full">
-        <img className="h-full" src={DualHero} alt="Dual Hero"></img>
+      <div className="flex flex-col flex-grow w-full md:w-1/2 h-auto">
+        <img className="w-full h-auto" src={DualHero} alt="Dual Hero"></img>
       </div>
     </section>
   );
